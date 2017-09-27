d71b78a0b3b937ea3b19e186f69e1ca3d15ea7d4
Steven Bird <stevenbird1@gmail.com>
2017-09-27 16:22:30 +0930

    include xml declaration and stylesheet

--- build.py
+++ build.py
@@ -7,7 +7,13 @@ lexicon = ToolboxData('bkwdictionarytest.txt').parse()
 elementtree_indent(lexicon)
 tree = ElementTree(lexicon)
 
+decl = """
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="dictionary.xsl"?>
+"""
+
 f = open('dictionary.xml', 'w')
+f.write(decl)
 tree.write(f, encoding='unicode')
 f.close()
 
