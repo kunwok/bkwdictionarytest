bcd34f62d86f5e5ef6a67f0a81a110a1416c9053
Steven Bird <stevenbird1@gmail.com>
2017-09-27 16:06:31 +0930

    add NLTK and test

--- build.py
+++ build.py
@@ -1,4 +1,13 @@
-f = open('hello.html', 'w')
-f.write('Hello World')
+from xml.etree.ElementTree import ElementTree
+
+from nltk.toolbox import ToolboxData
+from nltk.util import elementtree_indent
+
+lexicon = ToolboxData('bkwdictionarytest.txt').parse()
+elementtree_indent(lexicon)
+tree = ElementTree(lexicon)
+
+f = open('dictionary.xml', 'w')
+tree.write(f, encoding='unicode')
 f.close()
 
